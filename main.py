fc76e8b737ea6f828acf369078a93d73d1f5ae41
Coder731 <stephenobrien1@gmail.com>
2023-08-25 07:12:53 +0100

    Add main py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# From ChatGPT3.5
 import os
 from dotenv import load_dotenv
 
@@ -6,3 +7,9 @@ load_dotenv()
 
 # Now you can access the API key using os.getenv("POLYGON_API_KEY")
 api_key = os.getenv("POLYGON_API_KEY")
+
+# From Polygon.io
+ticker='AAPL'
+start_date='2023-01-09'
+end_date='2023-01-09'
+url='https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/day/{start_date}/{end_date}?apiKey={api_key}'
