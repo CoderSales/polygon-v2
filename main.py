e7489ddfe3d6635b912390f0c24d2a5a5884dbb6
Coder731 <stephenobrien1@gmail.com>
2023-08-25 07:15:29 +0100

    Add chat code forcall update requirements
    install requests

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-# From ChatGPT3.5
 import os
+import requests
 from dotenv import load_dotenv
 
 # Load variables from .env file
 load_dotenv()
 
-# Now you can access the API key using os.getenv("POLYGON_API_KEY")
+# Get your API key from environment variables
 api_key = os.getenv("POLYGON_API_KEY")
 
 # From Polygon.io
-ticker='AAPL'
-start_date='2023-01-09'
-end_date='2023-01-09'
-url='https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/day/{start_date}/{end_date}?apiKey={api_key}'
+ticker = 'AAPL'
+start_date = '2023-01-09'
+end_date = '2023-01-09'
+
+# Construct the complete URL with formatted strings
+url = f"https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/day/{start_date}/{end_date}?apiKey={api_key}"
+
+# Make the API request
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    data = response.json()
+    # Process the data as needed
+    print(data)
+else:
+    print(f"Request failed with status code: {response.status_code}")
